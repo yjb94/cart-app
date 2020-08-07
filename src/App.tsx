14a33b5c4367f4b7f63d6b577482cf3b1e19a0dd
yjb94 <jbyun94@naver.com>
2020-08-08 01:04:46 +0900

    Cart page routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import GlobalStyle from './styles/globalStyles';
 import { Switch, Route } from "react-router-dom";
 import Products from './pages/Products';
+import Cart from './pages/Cart';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <GlobalStyle />
       <Switch>
         <Route path="/products" component={Products} />
+        <Route path="/cart" component={Cart} />
       </Switch>
     </AppContainer>
   )
---
+++ src/pages/Cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart: React.FC = () => {
+  return (
+    <div>
+      cart
+    </div>
+  )
+};
+
+export default Cart;
